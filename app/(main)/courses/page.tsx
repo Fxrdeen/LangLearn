a3329b9b0741b5015faa15d555d230285a52b1ef
Fardeen S <fardeenclan@gmail.com>
2024-06-12 18:52:24 +0530

    final

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -4,6 +4,9 @@ import List from "./List";
 const CoursesPage = async () => {
   const courses = await getCourses();
   const userProgress = await getUserProgress();
+  if (!userProgress) {
+    return null;
+  }
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
